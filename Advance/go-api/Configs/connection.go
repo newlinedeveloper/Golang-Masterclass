cf859fd47b6eba6e3f8fb94e24b08506a6f4e9e8
newlinedeveloper <newlinedeveloper@gmail.com>
2023-04-01 16:17:33 +0530

    Mongodb Connection setup

---
+++ Advance/go-api/Configs/connection.go
@@ -0,0 +1,42 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// MongoDB connection function
+func ConnectDB() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB Connected successfully !!!! ")
+	return client
+}
+
+// // MongoDB Client instance
+// var DB *mongo.Client = ConnectDB()
+
+//Getting database collection
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("golang-masterclass").Collection(collectionName)
+	return collection
+}
--- Advance/go-api/main.go
+++ Advance/go-api/main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	configs "github.com/newlinedeveloper/go-api/Configs"
+
 	"github.com/gorilla/mux"
 )
 
-func getMessage(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"data": "Golang project setup test"})
-
-}
-
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", getMessage).Methods("GET")
-	fmt.Print("Server is running on port 8000 !!!")
-	log.Fatal(http.ListenAndServe(":8000", router))
 
+	// MongoDB Connection
+	configs.ConnectDB()
+
+	fmt.Print("Server is running on port 8000 !!!!")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
